c752af109b42c139b9c8ccc711840035b458e4bb
pranav2007 <raipranav71@gmail.com>
2026-03-13 16:01:37 +0530

    initialise frontend and fix backend routes

--- backend/src/routes/habitsRoutes.js
+++ backend/src/routes/habitsRoutes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { createHabit, deleteHabit, getAllHabits, markComplete } from "../controllers/habitsController.js"
+import { createHabit, deleteHabit, getAllHabits, getStats, markComplete, updateHabit } from "../controllers/habitsController.js"
 
 const router = express.Router()
 
 router.get("/", getAllHabits)
+router.get("/stats", getStats)
 router.post("/", createHabit)
 router.post("/:id/complete", markComplete)
+router.put("/:id", updateHabit)
 router.delete("/:id", deleteHabit)
 
 export default router
